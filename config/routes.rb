e727df604780fc212229ce6c85bee3c4453e49e8
Lazaro Herrera <lazherrera@gmail.com>
2017-04-09 22:29:05 -0400

    No need for installer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/resources/' => 'resource#index'
+  mount ResourceMonitor::Engine => '/resources'
   get '/stress/', to: 'page#stress'
   root 'page#index'
 end
