7d4eab21485a82ff88070ad8fd7e3b933e6f4541
Lazaro Herrera <lazherrera@gmail.com>
2017-04-10 02:05:03 -0400

    around_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :resource_monitor_app
-  after_action :resource_monitor_app
-
-  def resource_monitor_app
-    ResourceMonitor.benchmark(self)
-  end
+  around_action def benchmark; ResourceMonitor.benchmark(self); yield; ResourceMonitor.benchmark(self); end
 end
